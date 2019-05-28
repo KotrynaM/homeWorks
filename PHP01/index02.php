4d23a1f7a7c0617f6c20d7c01b9d239fef5bec18
KotrynaM <kotryna.maldutyte@gmail.com>
2019-05-28 21:38:07 +0300

    pridėtos paskaitos php01 užduotys

---
+++ PHP01/index02.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="lt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> classWork | php02</title>
+    </head>
+        <?php $šalis = 'Lietuva';
+        $miestas = 'Vilnius';
+        $populiacija = 574221;?>
+    <body>
+        <p><?= $miestas. ' Lietuvos sostinė.';?></p>
+        <p><?= "$miestas ir $šalis sparčiai auga.";?></p>
+    </body>
+</html>
---
+++ PHP01/index03.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> classWork | php03</title>
+    </head>
+        <?php $x = 10;
+        $y = 7;?>
+    <body>
+        <?php
+            $z = $x + $y;
+            echo "$x + $y= $z<br />";
+        
+            $z = $x - $y;
+            echo "$x - $y= $z<br />";
+        
+            $z = $x * $y;
+            echo "$x * $y= $z<br />";
+        
+            $z = $x / $y;
+            echo "$x / $y= $z<br />";
+        
+            $z = $x % $y;
+            echo "$x % $y= $z<br />";
+        ?>
+    </body>
+</html>
