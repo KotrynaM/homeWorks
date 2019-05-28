a5f19ec6ca997cb94dd9999a8ba46706ef4d9937
KotrynaM <kotryna.maldutyte@gmail.com>
2019-05-28 20:29:21 +0300

    Ikeltas php01

---
+++ PHP01/index01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="lt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> classWork | php01</title>
+    </head>
+        <?php $šalis = 'Lietuva';?>
+        <?php $miestas = 'Vilnius';?>
+        <?php $populiacija = 574221;?>
+    <body>
+        <ul>
+            <li><?= $šalis; ?></li>
+            <li><?= $miestas; ?></li>
+            <li><?= $populiacija; ?></li>
+        </ul>
+    </body>
+</html>
