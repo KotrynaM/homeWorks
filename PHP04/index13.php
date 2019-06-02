08846e4a905413f0fe8e4ea393c440fe15c069c8
KotrynaM <kotryna.maldutyte@gmail.com>
2019-06-02 22:51:26 +0300

    prideti index 13 ir 14

--- PHP04/index13.php
+++ PHP04/index13.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP14 | task14</title>
+    <title>PHP04 | task13</title>
     </head>
 
     <body>
---
+++ PHP04/index14.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP04 | task14</title>
+    </head>
+
+    <body>
+        <?php
+        
+            echo "&copy" . "2015-" . date("Y");
+                
+        ?>
+    </body>
+</html>
